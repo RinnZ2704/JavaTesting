10a07f2458e006c61eefadba2488400003dd1f39
Mikeneko <152618531+RinnZ2704@users.noreply.github.com>
2024-09-27 19:42:42 +0700

    Add files via upload

---
+++ ReusableCode/Method.java
@@ -0,0 +1,8 @@
+public class Main {
+    static void myMethod() {
+        System.out.println("My method is called: ");
+    }
+
+    public static void main(String[] args){
+        myMethod();
+    }
