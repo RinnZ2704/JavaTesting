7c3c43b7a554654b39fd3eb50ef9664b9c461d13
Mikeneko <152618531+RinnZ2704@users.noreply.github.com>
2024-09-28 21:47:01 +0700

    Delete HeThongDatPhongKhachSan/CacLoaiPhong.java

--- HeThongDatPhongKhachSan/CacLoaiPhong.java
+++
@@ -1,9 +0,0 @@
-class CacLoaiPhong extends KhachSan {
-
-  public void LoaiPhong {
-    System.out.println("Co 3 loai phong");
-  }
-
-  //Co nen de Abstract Class/Interface de hien thi thong tin phong?
-  
-}
