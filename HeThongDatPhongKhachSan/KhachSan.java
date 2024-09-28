70d3e3a19a97ce6984b5bfb937be5b71d1def8df
Mikeneko <152618531+RinnZ2704@users.noreply.github.com>
2024-09-28 13:48:48 +0700

    Create KhachSan.java

---
+++ HeThongDatPhongKhachSan/KhachSan.java
@@ -0,0 +1,9 @@
+class KhachSan {
+  public void ten() {
+    System.out.println("Chao mung quy khach");
+  }
+  public void nam() {
+    System.out.println("Khach San duoc xay dung vao nam 2024");
+  }
+}
+
