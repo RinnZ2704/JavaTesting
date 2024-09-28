368dbb8dad9e8e5a540cf760dcf8c28c04625a76
Mikeneko <152618531+RinnZ2704@users.noreply.github.com>
2024-09-28 21:46:37 +0700

    Add files via upload

---
+++ HeThongDatPhongKhachSan/Model/CacLoaiPhong.java
@@ -0,0 +1,9 @@
+class CacLoaiPhong extends KhachSan {
+
+  public void LoaiPhong {
+    System.out.println("Co 3 loai phong");
+  }
+
+  //Co nen de Abstract Class/Interface de hien thi thong tin phong?
+  
+}
