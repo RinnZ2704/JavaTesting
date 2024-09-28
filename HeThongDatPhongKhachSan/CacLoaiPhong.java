ee802f0e55b086aeadb73df311c6d5f4d1e9b640
Mikeneko <152618531+RinnZ2704@users.noreply.github.com>
2024-09-28 14:05:22 +0700

    Update CacLoaiPhong.java

--- HeThongDatPhongKhachSan/CacLoaiPhong.java
+++ HeThongDatPhongKhachSan/CacLoaiPhong.java
@@ -3,5 +3,7 @@ class CacLoaiPhong extends KhachSan {
   public void LoaiPhong {
     System.out.println("Co 3 loai phong");
   }
+
+  //Co nen de Abstract Class/Interface de hien thi thong tin phong?
   
 }
