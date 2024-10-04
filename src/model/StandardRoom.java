23142c9463fea014b53fba794e5d53b8dfb60d44
phamb <linh011186@stu.vinschool.edu.vn>
2024-10-04 21:50:06 +0700

    Constructor Update

--- src/model/StandardRoom.java
+++ src/model/StandardRoom.java
@@ -1,6 +1,10 @@
 package model;
 
 public class StandardRoom extends Room {
+    public StandardRoom(String roomNumber, boolean isBooked) {
+        super(roomNumber, isBooked);
+    }
+
     @Override
     public double getPrice() {
         return 10;
